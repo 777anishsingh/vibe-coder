18e833e7a3aaa7190710a58d63fe5d4e041bb947
777anishsingh <777anish.singh@gmail.com>
2025-07-07 21:50:46 +0530

    06: e2b-sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,27 +1,34 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-coder-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
 
     const codeAgent = createAgent({
       name: "code-helper",
-      system: "You are an expert software developer with skill involving MERN stack. you write redable, maintainable code and you only return code not any explanation but only code.",
-      model: gemini({ model: "gemini-2.0-flash"}),
+      system:
+        "You are an expert software developer with skill involving MERN stack. you write redable, maintainable code and you only return code not any explanation but only code.",
+      model: gemini({ model: "gemini-1.5-flash-8b" }),
     });
 
+    const { output } = await codeAgent.run(
+      `Write the coe for: ${event.data.value}`
+    );
 
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
 
-const { output } = await codeAgent.run(
-  `Write the coe for: ${event.data.value}`,
-);
-console.log(output);
-
-
-
-
-    return { output };
-  },
+    return { output, sandboxUrl };
+  }
 );
